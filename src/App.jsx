3264dc88aac01072cdde3cd96114be9977be9e06
Mateus Schverz <mate_fs@hotmail.com>
2023-09-26 19:45:09 -0300

    Chore: form de instrucoes aberto através de um modal
    adicionado modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,75 @@
-import { useState } from 'react';
-import './App.css';
-import ExtractFieldsFromLinesComponent from './components/ExtractFieldsFromLinesComponent';
-import UploadForm from './components/UploadForm';
-import LineInstructionsForm from './components/LineInstructionsForm';
-import {FloatButton } from 'antd'
+import { useState } from "react";
+import "./App.css";
+import ExtractFieldsFromLinesComponent from "./components/ExtractFieldsFromLinesComponent";
+import UploadForm from "./components/UploadForm";
+import LineInstructionsForm from "./components/LineInstructionsForm";
+import { FloatButton, Modal } from "antd";
 
 function App() {
   const [uploadedTextFile, setUploadedTextFile] = useState();
-  const [showLineInstructionsFormBoolean, setShowLineInstructionsFormBoolean] = useState(false)
+  const [showLineInstructionsFormBoolean, setShowLineInstructionsFormBoolean] =
+    useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
 
   const [lineInstructions, setLineInstructions] = useState([
     {
       startLine: 1,
       endLine: 1,
       fields: [
-        { name: 'Nome da empresa', startPos: 1, endPos: 27 },
-        { name: 'Ano atual', startPos: 28, endPos: 31 },
+        { name: "Nome da empresa", startPos: 1, endPos: 27 },
+        { name: "Ano atual", startPos: 28, endPos: 31 },
       ],
     },
     {
       startLine: 2,
       endLine: 10,
       fields: [
-        { name: 'Campo 1 ao 8', startPos: 1, endPos: 8 },
-        { name: 'Outro campo', startPos: 8, endPos: 28 },
+        { name: "Campo 1 ao 8", startPos: 1, endPos: 8 },
+        { name: "Outro campo", startPos: 8, endPos: 28 },
       ],
     },
   ]);
 
   return (
     <div>
-      <ExtractFieldsFromLinesComponent 
-      lineInstructions={lineInstructions} 
-      uploadedTextFile={uploadedTextFile}
+      <ExtractFieldsFromLinesComponent
+        lineInstructions={lineInstructions}
+        uploadedTextFile={uploadedTextFile}
       />
       <br />
       {}
       <UploadForm
         uploadedTextFile={uploadedTextFile}
-        setUploadedTextFile={setUploadedTextFile} 
+        setUploadedTextFile={setUploadedTextFile}
       />
-      <FloatButton onClick={() => {setShowLineInstructionsFormBoolean(!showLineInstructionsFormBoolean)}}/>
-      {showLineInstructionsFormBoolean && <LineInstructionsForm lineInstructions={lineInstructions}  setLineInstructions={setLineInstructions} />}
+      <FloatButton
+        onClick={() => {
+          setShowLineInstructionsFormBoolean(!showLineInstructionsFormBoolean);
+          showModal();
+        }}
+      />
+
+      <Modal
+        title="Editar intruções do arquivo"
+        width={"90%"}
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <LineInstructionsForm
+          lineInstructions={lineInstructions}
+          setLineInstructions={setLineInstructions}
+        />
+      </Modal>
     </div>
   );
 }
--- src/components/LineInstructionsForm.jsx
+++ src/components/LineInstructionsForm.jsx
@@ -1,28 +1,30 @@
-import React, { useState } from 'react';
-import { Input, Form, Button, Card, Typography } from 'antd';
+import React, { useState } from "react";
+import { Input, Form, Button, Card, Typography } from "antd";
 
 function LineInstructionsForm({ lineInstructions, setLineInstructions }) {
   const [startLine, setStart] = useState();
   const [endLine, setEndLine] = useState();
   const [addNewLineEmptyWithFieldsForm] = Form.useForm();
 
-  const [formNewFieldInsideLine] = Form.useForm()
-  const [newFieldName, setNewFieldName] = useState('');
+  const [formNewFieldInsideLine] = Form.useForm();
+  const [newFieldName, setNewFieldName] = useState("");
 
   const handleAddField = (lineIndex, newFieldName) => {
-    let lineInstructionNameAlreadyExist = lineInstructions[lineIndex].fields.some(
+    let lineInstructionNameAlreadyExist = lineInstructions[
+      lineIndex
+    ].fields.some(
       (item) => item.name.toLowerCase() == newFieldName.toLowerCase()
     );
 
     if (lineInstructionNameAlreadyExist) {
-      alert('Nome de campo ja existe');
+      alert("Nome de campo ja existe");
       return;
     } else {
       const updatedInstructions = [...lineInstructions];
       updatedInstructions[lineIndex].fields.push({
         name: newFieldName,
-        startPos: '',
-        endPos: '',
+        startPos: "",
+        endPos: "",
       });
       setLineInstructions(updatedInstructions);
     }
@@ -42,12 +44,12 @@ function LineInstructionsForm({ lineInstructions, setLineInstructions }) {
     });
 
     /* Validacao se ja existe ou se inicial é maior que final */
-    if(newLine.startLine > newLine.endLine ){
-      alert('A linha inicial não pode ser maior que a linha final ');
+    if (newLine.startLine > newLine.endLine) {
+      alert("A linha inicial não pode ser maior que a linha final ");
       return;
     } else {
       if (lineExists) {
-        alert('Essa linha já existe.');
+        alert("Essa linha já existe.");
         return;
       } else {
         setLineInstructions((prevInstructions) => [
@@ -56,119 +58,103 @@ function LineInstructionsForm({ lineInstructions, setLineInstructions }) {
         ]);
       }
     }
-
   };
 
   const handleSubmit = (values) => {
-
-    if(values.startLine == 0 || values.endLine  == 0 ){
-      alert('A linha inicial ou final não pode ser igual a 0')
-    }else{
-      
+    if (values.startLine == 0 || values.endLine == 0) {
+      alert("A linha inicial ou final não pode ser igual a 0");
+    } else {
       const newLine = {
-      startLine: values.startLine,
-      endLine: values.endLine,
-      fields: [],
-     };
+        startLine: values.startLine,
+        endLine: values.endLine,
+        fields: [],
+      };
 
       addLine(newLine);
-      setStart('');
-      setEndLine('');
+      setStart("");
+      setEndLine("");
       addNewLineEmptyWithFieldsForm.resetFields();
-
     }
-
   };
- 
-const handleFieldInitialFinalPosition = (event) => {
-  event.preventDefault();
 
-  let impedirSalvamento = false;
+  const handleFieldInitialFinalPosition = (event) => {
+    event.preventDefault();
 
-  let posicaoInicial = Number(event.target[0].value);
-  let posicaoFinal = Number(event.target[1].value);
-  let indiceLinha = Number(event.target[2].value);
-  let indiceCampo = Number(event.target[3].value);
+    let impedirSalvamento = false;
 
-  const updatedInstructions = [...lineInstructions];
-  let nomeCampoIndividual =
-    updatedInstructions[indiceLinha].fields[indiceCampo].name;
+    let posicaoInicial = Number(event.target[0].value);
+    let posicaoFinal = Number(event.target[1].value);
+    let indiceLinha = Number(event.target[2].value);
+    let indiceCampo = Number(event.target[3].value);
 
-  updatedInstructions[indiceLinha].fields.forEach((InteractedField) => {
+    const updatedInstructions = [...lineInstructions];
+    let nomeCampoIndividual =
+      updatedInstructions[indiceLinha].fields[indiceCampo].name;
 
-    /* Valida se em outros campos dessa mesma linha já existe essa posição inicial e final  */
-    if (InteractedField.name !== nomeCampoIndividual) {
-      if (
-        InteractedField.startPos === posicaoInicial ||
-        InteractedField.endPos === posicaoFinal
-      ) {
-        alert(
-          `Já existe um campo com essa posição inicial ou final no campo ${InteractedField.name}`
-        );
-        impedirSalvamento = true;
+    updatedInstructions[indiceLinha].fields.forEach((InteractedField) => {
+      /* Valida se em outros campos dessa mesma linha já existe essa posição inicial e final  */
+      if (InteractedField.name !== nomeCampoIndividual) {
+        if (
+          InteractedField.startPos === posicaoInicial ||
+          InteractedField.endPos === posicaoFinal
+        ) {
+          alert(
+            `Já existe um campo com essa posição inicial ou final no campo ${InteractedField.name}`
+          );
+          impedirSalvamento = true;
+        }
       }
+    });
 
+    if (posicaoInicial > posicaoFinal) {
+      alert("A posição inicial não pode ser maior do que a final");
+      impedirSalvamento = true;
     }
 
-  });
+    if (posicaoInicial == 0 || posicaoFinal == 0) {
+      impedirSalvamento = true;
+      alert("Posição inicial/final não pode ser 0");
+    }
 
-  if (posicaoInicial > posicaoFinal) {
-    alert('A posição inicial não pode ser maior do que a final');
-    impedirSalvamento = true;
-  }
-
-  if(posicaoInicial == 0  || posicaoFinal == 0 ){
-    impedirSalvamento = true;
-    alert('Posição inicial/final não pode ser 0')
-  }
-
-  if (impedirSalvamento) {
-    alert('Erro: a instrução não foi salva');
-  } else {
-    updatedInstructions[indiceLinha].fields[indiceCampo].startPos = posicaoInicial;
-    updatedInstructions[indiceLinha].fields[indiceCampo].endPos = posicaoFinal;
-    setLineInstructions(updatedInstructions);
-    alert('Instrução salva com sucesso!');
-  }
-};
-
-
+    if (impedirSalvamento) {
+      alert("Erro: a instrução não foi salva");
+    } else {
+      updatedInstructions[indiceLinha].fields[indiceCampo].startPos =
+        posicaoInicial;
+      updatedInstructions[indiceLinha].fields[indiceCampo].endPos =
+        posicaoFinal;
+      setLineInstructions(updatedInstructions);
+      alert("Instrução salva com sucesso!");
+    }
+  };
 
   return (
-<Card   style={{
-    position: 'fixed',
-    overflowY: 'auto',
-    top: 0,
-    left: 0,
-    width: '100%',
-    height: '100%',
-  }}>
-{/* <pre style={{ position: 'fixed', top: '10px', right: '10px', textShadow: '1px 1px white', zIndex: 999, maxHeight: '80vh', overflow: 'auto' }}>
+    <Card>
+      {/* <pre style={{ position: 'fixed', top: '10px', right: '10px', textShadow: '1px 1px white', zIndex: 999, maxHeight: '80vh', overflow: 'auto' }}>
   {JSON.stringify(lineInstructions, null, 2)}
 </pre>
  */}
 
-<div  >
+      <div>
         <h1>Adicionar nova linha</h1>
         <Form
           form={addNewLineEmptyWithFieldsForm}
           onFinish={handleSubmit}
-          initialValues={{ startLine: '', endLine: '' }}
-
+          initialValues={{ startLine: "", endLine: "" }}
         >
           <Form.Item
             label="Começo linha"
             name="startLine"
-            rules={[{ required: true, message: 'Campo obrigatório' }]}
-            defaultValue={''}
+            rules={[{ required: true, message: "Campo obrigatório" }]}
+            defaultValue={""}
           >
             <Input type="number" />
           </Form.Item>
           <Form.Item
             label="Fim linha"
             name="endLine"
-            rules={[{ required: true, message: 'Campo obrigatório' }]}
-            defaultValue={''}
+            rules={[{ required: true, message: "Campo obrigatório" }]}
+            defaultValue={""}
           >
             <Input type="number" />
           </Form.Item>
@@ -179,34 +165,38 @@ const handleFieldInitialFinalPosition = (event) => {
           </Form.Item>
         </Form>
       </div>
-      
-      
+
       <div>
         {lineInstructions.map((line, lineIndex) => (
           <Card
-            style={{ backgroundColor: 'rgba(0,0,0,.1)', marginTop: '2%' }}
+            style={{ backgroundColor: "rgba(0,0,0,.1)", marginTop: "2%" }}
             key={lineIndex}
           >
-
-          <section style={{display:'flex', gap:'10px'}}>
-            <Typography>
-              Linha de início: {line.startLine} | Linha final: {line.endLine}
-            </Typography>
-              <Button type='primary' 
-              style={{marginTop:'-0.4%'}} 
-              danger
-              onClick={() => {
-                const updatedInstructions = lineInstructions.filter((item) => item.startLine != line.startLine && item.endLine !== line.endLine)
-                setLineInstructions(updatedInstructions)
-                console.log(updatedInstructions) 
-
-              }} 
-              >Remover linha </Button>
-          </section>
+            <section style={{ display: "flex", gap: "10px" }}>
+              <Typography>
+                Linha de início: {line.startLine} | Linha final: {line.endLine}
+              </Typography>
+              <Button
+                type="primary"
+                style={{ marginTop: "-0.4%" }}
+                danger
+                onClick={() => {
+                  const updatedInstructions = lineInstructions.filter(
+                    (item) =>
+                      item.startLine != line.startLine &&
+                      item.endLine !== line.endLine
+                  );
+                  setLineInstructions(updatedInstructions);
+                  console.log(updatedInstructions);
+                }}
+              >
+                Remover linha{" "}
+              </Button>
+            </section>
 
             {line.fields.map((field, fieldIndex) => (
               <form
-                style={{marginTop:'1%'}}
+                style={{ marginTop: "1%" }}
                 key={fieldIndex}
                 onSubmit={(event) => handleFieldInitialFinalPosition(event)}
               >
@@ -215,53 +205,45 @@ const handleFieldInitialFinalPosition = (event) => {
                   <label>
                     Posição inicial:
                     <Input type="number" defaultValue={field.startPos} />
-                  </label> 
+                  </label>
                   <label>
                     Posição final:
                     <Input type="number" defaultValue={field.endPos} />
-                  </label> 
-                  <Input value={lineIndex} style={{ display: 'none' }} />
-                  <Input value={fieldIndex} style={{ display: 'none' }} />
-                  <Button type="primary" htmlType='submit' style={{margin: '1%'}}>Salvar instruções</Button>
+                  </label>
+                  <Input value={lineIndex} style={{ display: "none" }} />
+                  <Input value={fieldIndex} style={{ display: "none" }} />
+                  <Button
+                    type="primary"
+                    htmlType="submit"
+                    style={{ margin: "1%" }}
+                  >
+                    Salvar instruções
+                  </Button>
                 </Card>
               </form>
             ))}
 
-
-          <Form
-            form={formNewFieldInsideLine}
-            onFinish={(values) => {
-              handleAddField(lineIndex, values.newFieldName);
-              formNewFieldInsideLine.resetFields();
-
-            }}
-            initialValues={{ newFieldName: '' }}
-            style={{marginTop:'1%'}}
-          >
-            <Form.Item
-              name="newFieldName"
-              initialValue={''}
+            <Form
+              form={formNewFieldInsideLine}
+              onFinish={(values) => {
+                handleAddField(lineIndex, values.newFieldName);
+                formNewFieldInsideLine.resetFields();
+              }}
+              initialValues={{ newFieldName: "" }}
+              style={{ marginTop: "1%" }}
             >
-              <Input
-                type="text"
-                placeholder="Nome do novo campo"
-              />
-            </Form.Item>
-            <Form.Item>
-              <Button
-                type="primary"
-                htmlType="submit"
-              >
-                Adicionar novo campo
-              </Button>
-            </Form.Item>
-          </Form>
-
+              <Form.Item name="newFieldName" initialValue={""}>
+                <Input type="text" placeholder="Nome do novo campo" />
+              </Form.Item>
+              <Form.Item>
+                <Button type="primary" htmlType="submit">
+                  Adicionar novo campo
+                </Button>
+              </Form.Item>
+            </Form>
           </Card>
         ))}
       </div>
-
-
     </Card>
   );
 }
