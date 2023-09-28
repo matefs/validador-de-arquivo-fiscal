0f13ec2d9745efad687e69cbb09a4ecba674c212
Mateus Schverz <mate_fs@hotmail.com>
2023-09-27 23:02:06 -0300

    Update UploadForm.jsx

--- src/components/UploadForm.jsx
+++ src/components/UploadForm.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card} from 'antd';
+import { Card } from 'antd';
 
 export default function UploadForm({ uploadedTextFile, setUploadedTextFile }) {
   function carregarArquivoTexto(event) {
