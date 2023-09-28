b301d177c3a9605b22b748bb1b530854aff40528
Mateus Schverz <mate_fs@hotmail.com>
2023-09-27 23:00:43 -0300

    chore: removido componente upload que não era usado

--- src/components/UploadForm.jsx
+++ src/components/UploadForm.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Upload} from 'antd';
+import { Card} from 'antd';
 
 export default function UploadForm({ uploadedTextFile, setUploadedTextFile }) {
   function carregarArquivoTexto(event) {
